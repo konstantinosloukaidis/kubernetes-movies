6d485f66896a8c608f5847c082c632807df8142e
kloukaidis <konstantinosalouk@gmail.com>
2025-07-24 22:53:11 +0300

    CRUD Endpoints

--- backend/app/routes/movies.py
+++ backend/app/routes/movies.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from typing import Annotated
 
 from sqlmodel import Session, select
@@ -17,3 +17,41 @@ def get_all_movies(
     movies = db.exec(select(Movie)).all()
     return movies
 
+@router.get("/{movie_id}", response_model=Movie)
+def get_movie_by_id(
+    movie_id: int,
+    db: Session = Depends(get_session),
+) -> Movie:
+    movie = db.get(Movie, movie_id)
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return movie
+
+@router.post("/{movie_id}", response_model=Movie)
+def update_movie(
+    movie_id: int,
+    movie: Movie,
+    db: Session = Depends(get_session),
+) -> Movie:
+    existing_movie = db.get(Movie, movie_id)
+    if not existing_movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    
+    movie.id = movie_id
+    db.add(movie)
+    db.commit()
+    db.refresh(movie)
+    return movie
+
+@router.delete("/{movie_id}", response_model=Movie)
+def delete_movie(
+    movie_id: int,
+    db: Session = Depends(get_session),
+) -> Movie:
+    movie = db.get(Movie, movie_id)
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    
+    db.delete(movie)
+    db.commit()
+    return movie
