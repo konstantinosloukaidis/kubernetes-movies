26c0182ce4708c7dec5b05f7e6fd96861118ae2d
Konstantinos L. <konstantinosloukaidis@gmail.com>
2025-08-21 16:44:21 +0300

    Kubernetes fixes

--- kubectl_bash.sh
+++ kubectl_bash.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-kubectl apply -f postgresql-persistent-volume.yaml
-kubectl apply -f postgresql-persistent-volume-claim.yaml
-kubectl apply -f config.yaml
-kubectl apply -f secrets.yaml
-kubectl apply -f postgresql-init-script.yaml
-kubectl apply -f postgresql-service.yaml
-kubectl apply -f postgresql-deployment.yaml
+
+kubectl delete -f k8s/postgresql-deployment.yaml
+kubectl delete -f k8s/postgresql-service.yaml
+kubectl delete -f k8s/postgresql-init-script.yaml
+kubectl delete -f k8s/secrets.yaml
+kubectl delete -f k8s/config.yaml
+kubectl delete -f k8s/postgresql-persistent-volume-claim.yaml
+kubectl delete -f k8s/postgresql-persistent-volume.yaml
+
+kubectl apply -f k8s/postgresql-persistent-volume.yaml
+kubectl apply -f k8s/postgresql-persistent-volume-claim.yaml
+kubectl apply -f k8s/config.yaml
+kubectl apply -f k8s/secrets.yaml
+kubectl apply -f k8s/postgresql-init-script.yaml
+kubectl apply -f k8s/postgresql-service.yaml
+kubectl apply -f k8s/postgresql-deployment.yaml
 
